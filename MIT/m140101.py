e9c5a6b4125335006b28f7d948fe30f08a6552bf
harada <queryharada@gmail.com>
2019-07-23 19:13:22 +0900

    update

--- MIT/m140101.py
+++ MIT/m140101.py
@@ -3,6 +3,73 @@ class Location(object):
     def __init__(self, x, y):
         self.x, self.y = x, y
 
+    def move(self, deltaX, deltaY):
+        return Location(self.x + deltaX, self.y + deltaY)
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distFrom(self, other):
+        ox, oy = other.x, other.y
+        xDist, yDist = self.x - ox, self.y - oy
+        return (xDist ** 2 + yDist ** 2) ** 0.5
+
+    def __str__(self):
+        return '<' + str(self.x) + ',' + str(self.y) + '>'
+
+
+class Field(object):
+    def __init__(self):
+        self.drunks = dict()
+
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk ')
+        else:
+            self.drunks[drunk] = loc
+
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field ')
+        xDist, yDist = drunk.takeStep()
+        currentLocation = self.drunks[drunk]
+        self.drunks[drunk] = currentLocation.move(xDist, yDist)
+
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field ')
+        return self.drunks[drunk]
+
+
+import random
+
+
+class Drunk(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __str__(self):
+        if self != None:
+            return self.name
+        return 'Amonymous'
+
+
+class UsualDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        return random.choice(stepChoices)
+
+def walk(f, d, numSteps):
+    start = f.getLoc(d)
+    for s in range(numSteps):
+        f.moveDrunk(d)
+    return start.distFrom(f.getLoc(d))
+
+def simWalks(nuumSteps, numTrails, dClass):
+    pass
 
 if __name__ == "__main__":
     pass
