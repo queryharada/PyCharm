d7b2bcbf1090fafe588aec97e6dfa903e85b7fd2
harada <queryharada@gmail.com>
2019-08-05 15:23:55 +0900

    Update

--- MIT/m150102.py
+++ MIT/m150102.py
@@ -59,9 +59,65 @@ def flipPlot1(minExp, maxExp, numTrials):
     title = 'SD Heads/Tails Ratis ' + numTrialsString
     makePlot(xAxis, ratiosSDs, title, 'Number of flips ',
              'Standard Devision ', 'ko', logX=True, logY=True)
+    title = 'Mean abs(#Heads - #tails) ' + numTrialsString
+    makePlot(xAxis, diffMeans, title, 'Number of flips ',
+             'Mean abs(#Heads - #tails) ', 'ko', logX=True, logY=True)
+    title = 'SD abs(#Heads - #tails) ' + numTrialsString
+    makePlot(xAxis, diffsSDs, title, 'Number of flips ',
+             'Standard Devision ', 'ko', logX=True, logY=True)
+
+    pylab.show()
+
+
+def CV(X):
+    mean = sum(X) / len(X)
+    try:
+        return stdDev(X) / mean
+    except ValueError:
+        return float('nan')
+
+
+def flipPlot2(minExp, maxExp, numTrials):
+    ratiosMeans, diffMeans, ratiosSDs, diffsSDs = list(), list(), list(), list()
+    ratiosCVs, diffsCVs, xAxis = list(), list(), list()
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2 ** exp)
+    for numFlips in xAxis:
+        ratios, diffs = list(), list()
+        for t in range(numTrials):
+            numHeads, numTails = runTrial(numFlips)
+            ratios.append(numHeads / numTails)
+            diffs.append(abs(numHeads - numTails))
+        ratiosMeans.append(sum(ratios) / numTrials)
+        diffMeans.append(sum(diffs) / numTrials)
+        ratiosSDs.append(stdDev(ratios))
+        diffsSDs.append(stdDev(diffs))
+        ratiosCVs.append(CV(ratios))
+        diffsCVs.append(CV(diffs))
+    numTrialsString = '(' + str(numTrials) + ' Trials)'
+    title = 'Mean Heads/Tails Ratis ' + numTrialsString
+    makePlot(xAxis, ratiosMeans, title, 'Number of flips ',
+             'Mean Heads/Tails ', 'ko', logX=True)
+    title = 'SD Heads/Tails Ratis ' + numTrialsString
+    makePlot(xAxis, ratiosSDs, title, 'Number of flips ',
+             'Standard Devision ', 'ko', logX=True, logY=True)
+    title = 'Mean abs(#Heads - #tails) ' + numTrialsString
+    makePlot(xAxis, diffMeans, title, 'Number of flips ',
+             'Mean abs(#Heads - #tails) ', 'ko', logX=True, logY=True)
+    title = 'SD abs(#Heads - #tails) ' + numTrialsString
+    makePlot(xAxis, diffsSDs, title, 'Number of flips ',
+             'Standard Devision ', 'ko', logX=True, logY=True)
+
+    title = 'Coeff. of Var ' + numTrialsString
+    makePlot(xAxis, diffsCVs, title, 'Number of flips ',
+             'Coeff. of Var ', 'ko', logX=True, logY=True)
+
+    title = 'Coeff. of Var. Heads/Tails Ratis ' + numTrialsString
+    makePlot(xAxis, ratiosCVs, title, 'Number of flips ',
+             'Coeff. of Var. ', 'ko', logX=True, logY=True)
 
     pylab.show()
 
 
 if __name__ == "__main__":
-    flipPlot1(4, 20, 20)
+    flipPlot2(4, 20, 20)
