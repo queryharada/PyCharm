5855514effacd26123f7fff00621dc33d98fafa2
harada <queryharada@gmail.com>
2019-08-05 15:46:57 +0900

    Update

---
+++ MIT/m150201.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+import random
+import pylab
+
+vals = list()
+for i in range(1000):
+    num1 = random.choice(range(0, 101))
+    num2 = random.choice(range(0, 101))
+    vals.append(num1 + num2)
+pylab.hist(vals, bins=10)
+pylab.xlabel('Number of Occurrences ')
+
+pylab.show()
